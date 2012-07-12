e160fe35f22ef3e613cd4a56bbfcbf63b2905d0a
patricksebastien <patrick@11h11.com>
2012-07-12 13:50:17 -0400

    Unity project that use libpd

---
+++ unity/Assets/LibPDTests.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using LibPDBinding;
+using UnityEngine;
+
+public class LibPDTests : MonoBehaviour
+{
+	private static int SPatch;
+	
+	void Start()
+	{
+		loadPatch();
+		
+		
+		
+	}
+	
+	void Update()
+	{
+		testAudio();	
+	}
+	
+	void OnDestroy()
+	{
+		closePatch();
+	}
+    
+    public static void loadPatch()
+	{
+			LibPD.ReInit();
+
+			LibPD.OpenAudio(2, 3, 44100);
+			SPatch = LibPD.OpenPatch(Application.dataPath + "/test_csharp.pd");
+			LibPD.ComputeAudio(true);
+	}
+    
+    public static void closePatch()
+	{
+		LibPD.ClosePatch(SPatch);
+		LibPD.Release();
+	}
+    
+    public static void testAudio()
+	{
+		float[] inBuffer = new float[256];
+		float[] outBuffer = new float[256];
+		for (int i = 0; i < 256; i++)
+		{
+			inBuffer[i] = i;
+		}
+		int err = LibPD.Process(2, inBuffer, outBuffer);
+		Debug.Log(inBuffer[10]);
+		Debug.Log(outBuffer[10]);
+	}
+    
+}
